810053325a259d85f31f0aa50c2eb31e65704f54
Shrinivas Shukla <ssshukla1993@gmail.com>
2022-07-17 23:27:33 +0530

    Implemented reference resolver for Address

--- src/main/java/com/gql/gqladdressservice/GraphQLConfig.java
+++ src/main/java/com/gql/gqladdressservice/GraphQLConfig.java
@@ -2,6 +2,8 @@ package com.gql.gqladdressservice;
 
 import com.apollographql.federation.graphqljava.Federation;
 import com.apollographql.federation.graphqljava._Entity;
+import com.gql.gqladdressservice.domain.Address;
+import com.gql.gqladdressservice.resolver.QueryResolver;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.SchemaPrinter;
@@ -16,14 +18,29 @@ import java.util.stream.Collectors;
 public class GraphQLConfig {
 
     @Bean
-    public GraphQLSchema customSchema(SchemaParser schemaParser){
+    public GraphQLSchema customSchema(SchemaParser schemaParser, QueryResolver queryResolver){
         return Federation.transform(schemaParser.makeExecutableSchema())
-                .fetchEntities(env -> env.<List<Map<String, Object>>>getArgument(_Entity.argumentName)
-                        .stream()
-                        .map(values -> null)
-                        .collect(Collectors.toList()))
-                .resolveEntityType(env -> null)
-                .build();
+                .fetchEntities(env ->
+                        env.<List<Map<String, Object>>>getArgument(_Entity.argumentName)
+                                .stream()
+                                .map(values -> {
+                                    if ("Address".equals(values.get("__typename"))) {
+                                        final Object userId = values.get("userId");
+                                        if (userId instanceof Integer) {
+                                            return queryResolver.address((Integer) userId);
+                                        }
+                                    }
+                                    return null;
+                                })
+                                .collect(Collectors.toList())
+                )
+                .resolveEntityType(env -> {
+                    final Object src = env.getObject();
+                    if (src instanceof Address) {
+                        return env.getSchema().getObjectType("Address");
+                    }
+                    return null;
+                }).build();
     }
 
     @Bean
